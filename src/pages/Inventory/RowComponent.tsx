6159f6bf9aa3ffc293a06e3a4ae6d23f1dde76bf
Ediz Redzhepov <eredzhepov@vsgbg.com>
2023-06-05 16:54:48 +0300

    Added functionality for Lending and Returning items

--- src/pages/Inventory/RowComponent.tsx
+++ src/pages/Inventory/RowComponent.tsx
@@ -1,31 +1,14 @@
-import { TableCell, TableRow, tableCellClasses, styled, Fade } from '@mui/material';
-import { useState, useRef, Dispatch, SetStateAction } from 'react';
+import { useState, Dispatch, SetStateAction } from 'react';
 import { useDeleteProductMutation } from '../../services/productsService.ts';
 import { IProduct } from '../../types/types.ts';
 import { toast } from 'react-toastify';
+import { Fade } from '@mui/material'
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import EditProductModal from './EditProductModal.tsx';
+import AddHomeWorkIcon from '@mui/icons-material/AddHomeWork';
 import EditIcon from '@mui/icons-material/Edit';
 import Popup from '../../components/Popup.tsx';
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-        fontSize: 14,
-    },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-    },
-    '&:last-child StyledTableCell, &:last-child th': {
-        border: 0,
-    },
-}));
+import LendModal from './LendModal.tsx';
 
 type RowComponentProps = {
     product: IProduct;
@@ -35,8 +18,8 @@ type RowComponentProps = {
 const RowComponent = ({ product, setProducts }: RowComponentProps) => {
     const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
     const [showEditModal, setShowEditModal] = useState(false);
+    const [showLendModal, setShowLendModal] = useState(false);
     const [deleteProduct] = useDeleteProductMutation();
-    const rowRef = useRef<HTMLTableRowElement>(null);
 
     const handleDelete = async () => {
         setAnchorEl(null);
@@ -54,23 +37,20 @@ const RowComponent = ({ product, setProducts }: RowComponentProps) => {
     return (
         <>
             <EditProductModal setProducts={setProducts} product={product} showEditModal={showEditModal} setShowEditModal={setShowEditModal} />
+            <LendModal setProducts={setProducts} product={product} showLendModal={showLendModal} setShowLendModal={setShowLendModal} />
             <Popup popupMessage={popupMessage} onYes={handleDelete} anchorEl={anchorEl} setAnchorEl={setAnchorEl} />
-            <Fade in={true} timeout={500}>
-                <StyledTableRow ref={rowRef} id={product.id.toString()}>
-                    <StyledTableCell>{product.code}</StyledTableCell>
-                    <StyledTableCell>{product.fullName}</StyledTableCell>
-                    <StyledTableCell>{product.type}</StyledTableCell>
-                    <StyledTableCell>{product.quantityForSale || 0}</StyledTableCell>
-                    <StyledTableCell>{product.quantity}</StyledTableCell>
-                    <StyledTableCell>
-                        <a className='edit' onClick={() => setShowEditModal(true)}>
-                            <EditIcon />
-                        </a>
-                        <a className='delete' onClick={e => setAnchorEl(e.currentTarget)}>
-                            <DeleteOutlineIcon />
-                        </a>
-                    </StyledTableCell>
-                </StyledTableRow>
+            <Fade in={true} timeout={1000}>
+                <div>
+                    <a className='edit' onClick={() => setShowEditModal(true)}>
+                        <EditIcon />
+                    </a>
+                    <a className='lend' onClick={() => setShowLendModal(true)}>
+                        <AddHomeWorkIcon />
+                    </a>
+                    <a className='delete' onClick={e => setAnchorEl(e.currentTarget)}>
+                        <DeleteOutlineIcon />
+                    </a>
+                </div>
             </Fade>
         </>
     );
